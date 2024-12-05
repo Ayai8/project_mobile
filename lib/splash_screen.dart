9b1cb546913d0c3724ea4e714ca91878febaf94b
Ayai8 <115878745+Ayai8@users.noreply.github.com>
2024-12-05 22:10:00 +0200

    Add files via upload

---
+++ lib/splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+// Loading Screen
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MainScreen()));
+    });
+
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Sweet Recipes',
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.pink),
+        ),
+      ),
+    );
+  }
+}
